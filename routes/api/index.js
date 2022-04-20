3110539a0bb58b40a2451bc7b600afcbd57115d9
dkoshimo <d.koshimo@g.dreamcareer.co.jp>
2022-04-20 12:35:14 +0900

    API実装まで

--- routes/api/index.js
+++ routes/api/index.js
@@ -21,4 +21,16 @@ router.get("/tasks/:id", async function (req, res, next) {
   res.send(getTasksId);
 });
 
+/* タスク一件を更新するルーティング */
+router.patch("/tasks/:id", async function (req, res, next) {
+  const patchTasksId = await tasks.patchTasksId(req.params.id, req.body);
+  res.send(patchTasksId);
+});
+
+/* タスク一件を削除するルーティング */
+router.delete("/tasks/:id", async function (req, res, next) {
+  const deleteTasksId = await tasks.deleteTasksId(req.params.id);
+  res.send(deleteTasksId);
+});
+
 module.exports = router;
--- src/tasks.js
+++ src/tasks.js
@@ -56,6 +56,52 @@ getTasksId = async function (id) {
   }
 };
 
+/*タスク一件を更新する API*/
+patchTasksId = async function (id, body) {
+  let connection = null;
+  try {
+    connection = await mysql.createConnection(config.dbSetting);
+    const sql =
+      "UPDATE t_task SET task_name=?,deadline=?, category_id=?, task_status=?, updated_at=? WHERE id=?;";
+
+    let d = [
+      body.taskName,
+      body.deadline,
+      body.category,
+      body.status,
+      new Date(),
+      id,
+    ];
+    const [rows, fields] = await connection.query(sql, d);
+
+    return rows;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.end();
+  }
+};
+
+/*タスク一件を削除する API*/
+deleteTasksId = async function (id) {
+  let connection = null;
+  try {
+    connection = await mysql.createConnection(config.dbSetting);
+
+    const sql = "DELETE from t_task WHERE id = ?;";
+    let d = [id];
+    const [rows, fields] = await connection.query(sql, d);
+
+    return rows;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.end();
+  }
+};
+
 exports.postTasks = postTasks;
 exports.getTasks = getTasks;
 exports.getTasksId = getTasksId;
+exports.patchTasksId = patchTasksId;
+exports.deleteTasksId = deleteTasksId;
